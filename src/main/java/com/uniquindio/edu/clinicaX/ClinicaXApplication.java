e05330754a6c72eb75091d17c2eb7d321bb99c32
AFVA7 <andresf.valenciaa@uqvirtual.edu.co>
2023-09-08 21:01:19 -0500

    second commit

---
+++ src/main/java/com/uniquindio/edu/clinicaX/ClinicaXApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.clinicaX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/edu/clinicaX/ClinicaXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.clinicaX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
